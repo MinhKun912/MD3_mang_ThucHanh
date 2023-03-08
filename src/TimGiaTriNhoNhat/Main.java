ad0b7812525e3f7c2ec514edc536be1069c572d7
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-08 19:49:30 +0700

    first commit

---
+++ src/TimGiaTriNhoNhat/Main.java
@@ -0,0 +1,45 @@
+package TimGiaTriNhoNhat;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter array size :");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Khong duoc qua 20:");
+
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.println("enter Element" + (i + 1) + ":");
+            array[i] = scanner.nextInt();
+            i++;
+
+        }
+        System.out.println("List element off Array");
+
+        int j;
+        for (j = 0; j < array.length; j++) {
+            System.out.println(array[j] + "\t");
+
+        }
+        int min = 0;
+        int index = 1;
+        for (int k = 0; k < array.length; k++) {
+            min = array[0];
+
+            if (array[k] < min) {
+                min = array[k];
+                index = k + 1;
+
+            }
+        }
+        System.out.println("Min = " + min + " Vi tri " + index) ;
+}
+}
